a45aafb0dcc541db8b3e20222dccfe164b4f3ef8
zhouyijun <zhou495248579@hotmail.com>
2020-12-17 15:49:25 +0800

    install config

---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"),
+  config = require("./webpack.config");
+const plugins = config.plugins || [];
+module.exports = Object.assign({}, config, {
+  mode: "development",
+  devServer: {
+    open: true,
+  },
+  entry: {
+    example: "./example/animation/index.ts",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    ...plugins,
+    new HtmlWebpackPlugin({
+      template: "./example/animation/example1.html",
+    }),
+  ],
+});
